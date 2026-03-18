052393cf6c67e651739b6b1aaa5f08643fd54320
Sakshi <sakshiwadikar3105@gmail.com>
2026-03-19 00:14:38 +0530

    first

---
+++ Backend/models/User.js
@@ -0,0 +1,62 @@
+const mongoose= require("mongoose");
+
+const bcrypt = require("bcryptjs");
+
+const userSchema= new mongoose.Schema({
+    fullName:{
+        type: String,
+        required: true
+    },
+
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password:{
+        type: String,
+        required: true
+    },
+
+    profileImageUrl:{
+        type: String,
+        default: null
+    }
+
+},{
+    timestamps: true
+}
+
+);
+
+//Hash password before saving
+// userSchema.pre("save", async function(next){
+//     if(!this.isModified("password")) return next();
+//     this.password= await bcrypt.hash(this.password, 10);
+//     next();
+
+// });
+// userSchema.pre("save", function(next){
+//     if(!this.isModified("password")) return next();
+
+//     bcrypt.hash(this.password, 10)
+//         .then(hash => {
+//             this.password = hash;
+//             next();
+//         })
+//         .catch(err => next(err));
+// });
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+//Compare password
+userSchema.methods.comparePassword= async function(candidatePassword){
+    return await bcrypt.compare(candidatePassword, this.password);
+}
+
+module.exports= mongoose.model("User", userSchema);
---
+++ Frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
+const App=()=>{
+  return (
+    <div>
+      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+          
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+const Root =() =>{
+  //check if user is authenticated
+  const isAuthenticated = !!localStorage.getItem('token');
+  
+  //redirect to dashboard if authenticated, otherwise redirect to login
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+}
