a1c4148a241209455b22deca091be38b94bf1661
Sakshi Wadikar <sakshiwadikar3105@gmail.com>
2026-03-27 00:40:04 +0530

    changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,8 +1,23 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//     try{
+//         await mongoose.connect(process.env.MONGO_URI, {});
+//         console.log("MongoDB connected successfully");
+//     } catch (error){
+//         console.error("MongoDB connection failed:", error.message);
+//         process.exit(1);
+//     }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI, {});
+        console.log("Mongo URI:", process.env.MONGO_URI); // 👈 add this line
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected successfully");
     } catch (error){
         console.error("MongoDB connection failed:", error.message);
